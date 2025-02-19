a698645109c3e6b0e7c0ffa7d675d975afb9cf85
048525 <048525@UNI-LAB2-44>
2025-02-18 21:47:18 -0300

    Adicionar arquivos de projeto.

---
+++ Aula 18-02-2025/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] vetor;
+int i, maior_numero, numeros;
+
+Console.WriteLine("Digite o tamanho do vetor");
+numeros = int.Parse(Console.ReadLine());
+vetor = new int[numeros];
+
+for (i = 0; i < numeros; i++)
+{
+    Console.WriteLine($"Digite {numeros} numeros");
+    vetor[i] = int.Parse(Console.ReadLine());   
+}
+maior_numero = Math.Max();
+
+Console.WriteLine($"O maior numero é {maior_numero}");
+
