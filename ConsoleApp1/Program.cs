d63f2434cbea857af9187f89883ac326e4f39c43
Caio <Caio@DESKTOP-L2PJN2K>
2025-03-03 16:02:39 -0300

    atividade nelio ok

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,12 +1,12 @@
 ﻿int[] vetor;
-int i, numeros, soma = 0, soma_par=0;
+int i, numeros, soma_par = 0, quantidade_pares = 0;
 double media;
 
 Console.WriteLine("Digite o tamanho do vetor");
 numeros = int.Parse(Console.ReadLine());
 vetor = new int[numeros];
 
-Console.WriteLine($"Digite {numeros} numeros");
+Console.WriteLine($"Digite {numeros} números");
 for (i = 0; i < numeros; i++)
 {
     vetor[i] = int.Parse(Console.ReadLine());
@@ -14,11 +14,19 @@ for (i = 0; i < numeros; i++)
 
 for (i = 0; i < numeros; i++)
 {
- 
+    if (vetor[i] % 2 == 0)
+    {
+        soma_par += vetor[i];
+        quantidade_pares++;
+    }
 }
 
-numeros = vetor.Length;
-
-media = soma / numeros;
-
-Console.WriteLine($"A média desses numeros é: {media}");
+if (quantidade_pares > 0)
+{
+    media = (double)soma_par / quantidade_pares;
+    Console.WriteLine($"A média dos números pares é: {media}");
+}
+else
+{
+    Console.WriteLine("Não há números pares para calcular a média.");
+}
